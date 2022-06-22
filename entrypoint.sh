1e402767d682f2f8652d88fbfcb0e6b192e346bc
Rafael Almeida <rafaelcpalmeida@users.noreply.github.com>
2022-06-22 16:34:56 +0100

    Update Artifactory auth details (#2)

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ python -m pip install --upgrade pip --quiet 2>&1 1>/dev/null
 
 if [ -f pyproject.toml ]; then
     pip install poetry --quiet 2>&1 1>/dev/null 
-    POETRY_HTTP_BASIC_ARTIFACTORY_USERNAME="$EXTRA_INDEX_URL_PULL_TOKEN" poetry export -f requirements.txt --output requirements.txt --without-hashes --quiet 2>&1 1>/dev/null
+    POETRY_HTTP_BASIC_ARTIFACTORY_USERNAME="$EXTRA_INDEX_URL_USERNAME" POETRY_HTTP_BASIC_ARTIFACTORY_PASSWORD="$EXTRA_INDEX_URL_PASSWORD" poetry export -f requirements.txt --output requirements.txt --without-hashes --quiet 2>&1 1>/dev/null
 fi
 
 python -m venv env
@@ -22,7 +22,7 @@ python -m venv env
 # Update pip inside the virtual environment
 python -m pip install --upgrade pip --quiet 2>&1 1>/dev/null
 
-pip install --no-cache-dir -r requirements.txt --extra-index-url https://"$EXTRA_INDEX_URL_PULL_TOKEN":@"$EXTRA_INDEX_URL" --quiet 2>&1 1>/dev/null 
+pip install --no-cache-dir -r requirements.txt --extra-index-url https://"$EXTRA_INDEX_URL_USERNAME":"$EXTRA_INDEX_URL_PASSWORD"@"$EXTRA_INDEX_URL" --quiet 2>&1 1>/dev/null 
 
 if [ -f allowed_dependencies.txt ]; then
     while IFS="" read -r DEPENDENCY || [ -n "$DEPENDENCY" ]
