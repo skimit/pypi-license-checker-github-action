7674fb28cfab4e9d83ff9d92d32fa2bb7bb7c3a8
Vitor Oliveira <101562380+vhroliveira@users.noreply.github.com>
2023-01-23 17:15:55 +0000

    create temp log file (#4)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+touch ~/cmd.log
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap 'echo "\"${last_command}\" command exited with exit code $?."; cat ~/cmd.log' EXIT
 
